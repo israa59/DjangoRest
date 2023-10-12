2e532491373936d9287f641d6e188b9a2629f324
Israa Irshad <israa@qburst.com>
2023-10-12 17:40:38 +0530

    FBV for Creating a Student

--- for_serializer/views.py
+++ for_serializer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import response
+from rest_framework import response, status
 from for_serializer.models import Student
 from for_serializer.serializers import StudentSerializer
 
@@ -9,3 +9,10 @@ def student_request(request):
           students = Student.objects.all()
           serializer = StudentSerializer(students, many=True)
           return response.Response(serializer.data)
+     
+     elif request.method == 'POST':
+          serializer = StudentSerializer(data=request.data)
+          if serializer.is_valid():
+               serializer.save()
+               return response.Response(serializer, status= status.HTTP_201_CREATED)
+          return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
