a613d1bd823c8bdabc87cde206618a5609fc410b
Israa Irshad <israa@qburst.com>
2023-10-12 14:11:11 +0530

    Updated database to postgres

--- firstApp/models.py
+++ firstApp/models.py
@@ -4,5 +4,5 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     salary = models.DecimalField(max_digits=10, decimal_places=3)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.id+self.name+self.salary
