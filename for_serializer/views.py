8a2f13c58771b5f64d2a40b3f693bf8dd8d266ab
Israa Irshad <israa@qburst.com>
2023-10-12 17:26:53 +0530

    Created studentl list FBV

--- for_serializer/views.py
+++ for_serializer/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from rest_framework import response
+from for_serializer.models import Student
+from for_serializer.serializers import StudentSerializer
+
+def student_request(request):
+     
+     if request.method == 'GET':
+          students = Student.objects.all()
+          serializer = StudentSerializer(students, many=True)
+          return response.Response(serializer.data)
