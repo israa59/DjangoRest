dcd2f22fb9a52a362aebc1d539469dca54d222d5
Israa Irshad <israa@qburst.com>
2023-10-12 14:04:55 +0530

    configured urls for employee

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def employeeView(request):
+    emp = {
+        'id' : 123,
+        'name' : 'John',
+        'salary' : 10000,
+    }
+    return JsonResponse(emp)
